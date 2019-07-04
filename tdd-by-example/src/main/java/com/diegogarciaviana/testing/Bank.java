97177fede380add00dfada72ec6aebf0032245a5
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-07-04 17:25:31 +0200

    Testing with Spring

--- tdd-by-example/src/main/java/com/diegogarciaviana/testing/Bank.java
+++ tdd-by-example/src/main/java/com/diegogarciaviana/testing/Bank.java
@@ -1,17 +1,27 @@
 package com.diegogarciaviana.testing;
 
+import java.util.HashMap;
+
 public class Bank {
+
+    // Like a list
+    private HashMap<Pair, Integer> rateMap = new HashMap();
 
     public Money reduce(Expression source, String toCurrency) {
         return source.reduce(this, toCurrency);
     }
 
     public void addRate(String from, String to, int rate) {
-
+        rateMap.put(new Pair(from, to), rate);
     }
 
-    public float rate(String from, String to) {
-        return from.equals("CHF") && to.equals("USD") ? 2:1;
+    public int rate(String from, String to) {
+
+        if (from.equals(to))
+            return 1;
+
+        return rateMap.get(new Pair(from, to));
+
     }
 
 }
