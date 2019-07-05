517ac9c720bcab5fdb040ef7665bd3a56d56481f
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-07-05 14:08:44 +0200

    Testing with Spring

--- introjunit5/src/test/java/com/diegogarciaviana/app/GreetingTest.java
+++ introjunit5/src/test/java/com/diegogarciaviana/app/GreetingTest.java
@@ -1,21 +1,48 @@
 package com.diegogarciaviana.app;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class GreetingTest {
 
+    private Greeting greeting;
+
+    // Before all the tests...
+    @BeforeAll
+    public static void beforeClass() {
+        System.out.println("In Before All...");
+    }
+
+    // Before each test...
+    @BeforeEach
+    public void setUp() {
+        System.out.println("In Before Each...");
+        greeting = new Greeting();
+    }
+
     @Test
     public void testHelloWorld() {
-        Greeting greeting = new Greeting();
         assertEquals(greeting.helloWorld(), "Hello World");
     }
 
     @Test
     public void testHelloName() {
-        Greeting greeting = new Greeting();
         assertEquals(greeting.helloWorld("Diego"), "Hello Diego");
+        assertNotEquals(greeting.helloWorld("Diego"), "Hello Javi");
+    }
+
+    // After each test...
+    @AfterEach
+    public void tearDown() {
+        System.out.println("In After Each...");
+    }
+
+    // After all the tests...
+    @AfterAll
+    public static void afterClass() {
+        System.out.println("In After All....");
     }
 
 }
