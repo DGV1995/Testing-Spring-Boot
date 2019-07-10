46ea31cd380898f5ad886c923bee1bc87095b08a
DGV1995 <diegogarciaviana.1995@gmail.com>
2019-07-10 17:46:59 +0200

    Testing with Spring

--- testing-junit5-mockito/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ testing-junit5-mockito/src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -7,11 +7,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
@@ -19,25 +17,44 @@ import static org.mockito.BDDMockito.then;
 @ExtendWith(MockitoExtension.class)
 class OwnerControllerTest {
 
+    private static final String OWNERS_CREATE_OR_UPDATE_OWNER_FORM = "owners/createOrUpdateOwnerForm";
+    private static final String REDIRECT_OWNERS_1 = "redirect:/owners/1";
+
     @Mock
     private OwnerService ownerService;
+
+    @Mock
+    private BindingResult result;
 
     @InjectMocks
     private OwnerController ownerController;
 
     @Test
-    void processCreationForm() {
+    void processCreationFormHasErrors() {
         // Given
         Owner owner = new Owner(1L, "Joe", "Buck");
-        BindingResult result;
-        given(ownerService.save(any(Owner.class))).willReturn(any(Owner.class));
+        given(result.hasErrors()).willReturn(true);
 
         // When
         String view = ownerController.processCreationForm(owner, result);
 
         // Then
-        assertThat(view).isEqualTo("redirect:/owners/" + owner.getId());
-        then(ownerService).should().save(any(Owner.class));
-
+        assertThat(view).isEqualToIgnoringCase(OWNERS_CREATE_OR_UPDATE_OWNER_FORM);
     }
+
+    @Test
+    void processCreationFormNoErrors() {
+        // Given
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        given(result.hasErrors()).willReturn(false);
+        given(ownerService.save(any(Owner.class))).willReturn(owner);
+
+        // When
+        String view = ownerController.processCreationForm(owner, result);
+
+        // Then
+        assertThat(view).isEqualToIgnoringCase(REDIRECT_OWNERS_1);
+        then(ownerService).should().save(any(Owner.class));
+    }
+
 }
